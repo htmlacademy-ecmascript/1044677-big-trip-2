f67fe2f813d38d910f7ce0167614fa5280fe83fe
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2024-12-17 18:13:47 +0300

    "Включит" обновление задач на сервере

--- src/model/event-points-model.js
+++ src/model/event-points-model.js
@@ -64,19 +64,26 @@ export default class EventPointsModel extends Observable {
     return allDestinations.find((destination) => destination.id === id);
   }
 
-  updatePoint(updateType, updatedPoint) {
+  async updatePoint(updateType, updatedPoint) {
     const index = this.#points.findIndex((point) => point.id === updatedPoint.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
 
-    this.#points = [
-      ...this.#points.slice(0, index),
-      updatedPoint,
-      ...this.#points.slice(index + 1),
-    ];
-    this._notify(updateType, updatedPoint.id);
+    try {
+      const response = await this.#eventPointsApiService.updatePoint(updatedPoint);
+      const updated = this.#adaptToClient(response);
+
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updated,
+        ...this.#points.slice(index + 1),
+      ];
+      this._notify(updateType, updated.id);
+    } catch(err) {
+      throw new Error('Can\'t update point');
+    }
   }
 
   addPoint(updateType, newPoint) {
